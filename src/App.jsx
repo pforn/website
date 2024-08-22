942861027766a94ac635cb18fe6170d216db7d8a
Peter Fornasiero <peteforn@gmail.com>
2024-08-22 10:59:01 -0400

    Added Bulma

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section className="section">
+        <div className="container">
+          <div className="columns is-mobile is-centered">
+            <div className="column is-narrow">
+              <a
+                href="https://vitejs.dev"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={viteLogo} className="logo" alt="Vite logo" />
+              </a>
+            </div>
+            <div className="column is-narrow">
+              <a
+                href="https://react.dev"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={reactLogo} className="logo react" alt="React logo" />
+              </a>
+            </div>
+          </div>
+          <h1 className="title">Vite + React</h1>
+          <div className="card">
+            <div className="card-content">
+              <div className="content">
+                <button
+                  className="button is-primary"
+                  onClick={() => setCount((count) => count + 1)}
+                >
+                  Count is {count}
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
