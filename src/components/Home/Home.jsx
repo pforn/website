8cb1bde3de6088595601353aea367dfec96f2681
Peter Fornasiero <peteforn@gmail.com>
2024-09-04 12:41:01 -0400

    Changed text

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to My Personal Website</h1>
-      <p>This is the landing page.</p>
+      <h1>I got it to work yay</h1>
+      <p>Hi Rana</p>
     </div>
   );
 };
